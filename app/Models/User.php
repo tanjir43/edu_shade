930bfc1dfe34f0b94a069e65567084151d2dc6e7
tanjir43 <tanjirislam7@gmail.com>
2025-02-16 01:03:40 +0600

    Role permission added with seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
     protected $fillable = [
         'name',
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+    */
+    public function run(): void
+    {
+        $roles = [
+            'Super admin',
+            'Admin',
+            'Teacher',
+            'Student',
+            'Parents',
+            'Accountant',
+            'Receptionist',
+            'Librarian',
+            'Driver',
+            'Alumni',
+            "Hostel manager",
+            "Medical officer",
+            "IT administrator",
+            "Sports coach",
+            "Lab Assistant"
+        ];
+
+        foreach ($roles as $roleName) {
+            Role::updateOrCreate(
+                [
+                    'name' => $roleName,
+                    'branch_id' => 1,
+                    'school_id' => 1,
+                ],
+                [
+                    'type'       => 'system',
+                    'created_at' => Carbon::now(),
+                ]
+            );
+        }
+    }
+}
